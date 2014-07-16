bdae89bd79e6f94dcb64cacb03490a414b9758c1
zhoufudong <hzzhoufudong@corp.netease.com>
2014-07-16 18:54:35 +0800

    new branch of newbranch

--- TestZFDBanner/TestZFDBanner/ZFDAppDelegate.h
+++ TestZFDBanner/TestZFDBanner/ZFDAppDelegate.h
@@ -10,6 +10,6 @@
 
 @interface ZFDAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIWindow *window;//asfdasf
 
 @end
